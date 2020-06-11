083bc76f673ee29cf3ad27f55f242aa15a07fd8e
Vinicius <vinicius.cruz@estudante.ufla.br>
2020-06-11 11:59:00 -0300

    Criação da Classe D

---
+++ D.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class D{
+	private:
+		int D1;
+		float D2;
+	public:
+		int getD1(){ return D1; }
+		float getD2() { return D2; }
+		void setD1(int n) { D1 = n; }
+		void setD2(float n) { D2 = n; }
+	
+		void MD1() { cout << "MD1" << endl; }
+		void MD2() { cout << "MD2" << endl; }
+};
